25bd32693351a06d24e639b7593ae38e3b63fe90
jrodriguez <jrodriguez@novosit.com>
2020-10-06 19:43:35 -0400

    Added code for sync document

--- Controllers/DocumentController.cs
+++ Controllers/DocumentController.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using DocumentSender;
 
 namespace SenderDocumentSync.Controllers
 {
     [RoutePrefix("document")]
     public class DocumentController : ApiController
     {
+        readonly SendDocumentToOnBase send = new SendDocumentToOnBase();
 
         [Route("send-document/{handler}")]
         [HttpPost]
         public IHttpActionResult SendDocumentToOnBase(long handler)
         {
+            var jdoc = Newtonsoft.Json.JsonConvert.DeserializeObject("{}");
+
+            send.SaveDocument(handler, new OnBaseReleaser());
             return Ok();
         }
 
@@ -22,7 +28,15 @@ namespace SenderDocumentSync.Controllers
         [HttpGet]
         public IHttpActionResult GetDocumentMapping()
         {
-            return Ok();
+            var keywords = send.CastSettingToKeywords();
+            //var keywords = ConfigurationManager.AppSettings["keywords"].Split(';')
+            //    .Select(x =>
+            //{
+            //    string[] currentPair = x.Split(',');
+            //    return new KeyValuePair<string, string>(currentPair[0], currentPair[1]);
+            //}).ToDictionary(y => y.Key, z => z.Value);
+
+            return Ok(keywords);
         }
 
     }
